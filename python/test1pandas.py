05851d1334a327c106eafd0f540909fd997f1170
Melissa de Sousa <melissasousa6ms@gmail.com>
2023-02-01 11:12:32 -0300

    Update file

--- python/test1pandas.py
+++ python/test1pandas.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import seaborn as sns
+import statistics as sts
 
-dados = pd.read_csv("")
+dados = pd.read_csv("dados/Churn.csv", sep=";")
+print(dados.head())
+
+print(dados.shape)
+
+dados.columns = ["Id", "Score", "Estado", "Genero", "Idade", "Patrimonio", "Saldo", "Produtos", "TemCartCredito", "Ativo", "Salario", "Saiu"]
+
+print(dados.head())
+
+
+
